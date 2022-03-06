91934261e77ff023bd5ecc9ceaeede02f0a1b555
matthewds02 <descheppermatthew@gmail.com>
2022-03-06 15:54:12 +0100

    begin of application

---
+++ src/main/java/be/thomasmore/eindopdracht/EindOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.eindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindOpdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindOpdrachtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/eindopdracht/EindOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.eindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindOpdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
