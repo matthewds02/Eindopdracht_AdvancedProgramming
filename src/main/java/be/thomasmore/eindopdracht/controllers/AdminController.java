0de46d59146794a51c0bfd69c9846d9cc1797841
matthewds02 <descheppermatthew@gmail.com>
2022-04-07 12:58:21 +0200

    edit button on detailspage

--- src/main/java/be/thomasmore/eindopdracht/controllers/AdminController.java
+++ src/main/java/be/thomasmore/eindopdracht/controllers/AdminController.java
@@ -1,9 +1,10 @@
-/*
 package be.thomasmore.eindopdracht.controllers;
 
 import be.thomasmore.eindopdracht.model.Game;
 import be.thomasmore.eindopdracht.repositories.GameRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +12,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     private Logger logger = LoggerFactory.getLogger(AdminController.class);
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @GetMapping("/gamesedit/{id}")
     public String gamesEdit(Model model, @PathVariable int id) {
         logger.info("gamesedit"+id);
-        Optional<Game> optionalGame = GameRepository.findById(id);
+        Optional<Game> optionalGame = gameRepository.findById(id);
         if (optionalGame.isPresent()) {
             model.addAttribute("game", optionalGame.get());
         }
         return "admin/gamesedit";
     }
 }
-*/
